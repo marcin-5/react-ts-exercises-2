73c5d2ce14ae02b56b12202f4d43aa5a28df22fb
Marcin Bojara <marcin_5@mail.ru>
2023-12-12 19:23:48 +0100

    add basic reducer function and basic action type

--- src/store/timer-context.tsx
+++ src/store/timer-context.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, type ReactNode } from 'react';
+import { createContext, useContext, useReducer, type ReactNode } from 'react';
 
 type Timer = {
   name: string;
@@ -8,6 +8,11 @@ type Timer = {
 type TimersState = {
   isRunning: boolean;
   timers: Timer[];
+};
+
+const initialState: TimersState = {
+  isRunning: true,
+  timers: [],
 };
 
 type TimerContextValue = TimersState & {
@@ -32,18 +37,29 @@ type TimersContextProviderProps = {
   children: ReactNode;
 };
 
+type Action = {
+  type: 'ADD_TIMER' | 'START_TIMERS' | 'STOP_TIMERS';
+};
+
+// generate new state
+function timersReducer(state: TimersState, action: Action): TimersState {
+  
+}
+
 export default function TimersContextProvider({ children }: TimersContextProviderProps) {
+  const [timersState, dispatch] = useReducer(timersReducer, initialState);
+
   const ctx: TimerContextValue = {
     timers: [],
     isRunning: false,
     addTimer(timerData) {
-      //
+      dispatch({ type: 'ADD_TIMER' });
     },
     startTimer() {
-      //
+      dispatch({ type: 'START_TIMERS' });
     },
     stopTimers() {
-      //
+      dispatch({ type: 'STOP_TIMERS' });
     },
   };
   return <TimersContext.Provider value={ctx}>{children}</TimersContext.Provider>;

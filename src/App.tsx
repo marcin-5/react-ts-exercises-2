cf6908adea42769a6ae576edf3e607a67a5912f9
Marcin Bojara <marcin_5@mail.ru>
2023-12-12 15:19:32 +0100

    build polymorphic component with generics

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import Input from './components/Input';
+import Button from "./components/Button";
+import Container from "./components/Container.tsx";
 
 function App() {
   return (
     <main>
-      <Input id="name" label="Your name" type="text" />
-      <Input id="age" label="Your age" type="number" />
+      <Container as={Button}>Click Me</Container>
     </main>
   );
 }
